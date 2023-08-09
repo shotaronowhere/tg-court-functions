57bc09610c87785c9101d30e647bf2c18fbb31b1
shotaronowhere <shawtarohgn@gmail.com>
2023-08-09 21:33:07 +0100

    feat: dispute notifications

--- functions/cron.ts
+++ functions/cron.ts
@@ -3,12 +3,9 @@ import { datalake } from "../config/supabase";
 import { StatusCodes } from "http-status-codes";
 import { Handler, schedule } from "@netlify/functions";
 
-const headers = {
-    "Access-Control-Allow-Origin": "*",
-};
-
 const handler: Handler = async () => {
     try{
+
         const { data, error } = await datalake
         .from(`bot-block-heights`)
         .select("*")
@@ -22,43 +19,77 @@ const handler: Handler = async () => {
                 body: JSON.stringify({ error: error.message }),
             };
 
-        const blockHeight = data[0].block_height
-        const timestamp = data[0].timestamp
-
-        const disputeQuery = `
-        query {
-            disputes(first: 1000, where: {startTime_gt: 0}, orderBy: disputeID) {
-                disputeID
-              arbitrable {
-                id
-              }
-              subcourtID{
-                id
-              }
+        const timestampOld = data[0].timestamp
+        let startTimeMax = timestampOld;
+        const disputeQuery = {
+            query: `{
+                disputes(first: 10, where: {startTime_gt: ${timestampOld}}, orderBy: disputeID) {
+                    numberOfVotes
+                    startTime
+                    disputeID
+                arbitrable {
+                    id
                 }
-            }`
+                subcourtID{
+                    id
+                  policy{
+                    policy
+                  }
+                  
+                }
+                }
+             }`,
+        }
 
-        const response = await axios.post(`https://api.thegraph.com/subgraphs/name/salgozino/klerosboard`, {
-            body: JSON.stringify({disputeQuery}),
-            header: {
-                contentType: "application/json"
+        
+
+        const response = (await axios.post(`https://api.thegraph.com/subgraphs/name/salgozino/klerosboard`,    
+            disputeQuery)).data.data.disputes
+        for(const dispute of response){
+            if (dispute.startTime > startTimeMax){
+                startTimeMax = dispute.startTime
             }
-        });
+            const res = await axios.get(`https://ipfs.kleros.io/${dispute.subcourtID.policy.policy}`);
+    
+            const queryTag = {
+                query: ` {
+              addressTags: litems(where:{
+                registry:"0x66260c69d03837016d88c9877e61e08ef74c59f2",
+                key0_starts_with_nocase: "eip155:1:${response[0].arbitrable.id}",
+                key0_ends_with_nocase: "eip155:1:${response[0].arbitrable.id}",
+                status_in:[Registered, ClearingRequested]
+              }, first: 1) {
+                key0
+                key1
+                key2
+                key3
+              }
+            }
+            `};
+            const responseTag = (await axios.post('https://api.thegraph.com/subgraphs/name/kleros/legacy-curate-xdai',    
+            queryTag)).data.data.addressTags
+    
 
-        console.log(response)
+            await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+                chat_id: process.env.NOTIFICATION_CHANNEL,
+                text: `[Dispute ${dispute.disputeID}](https://court.kleros.io/cases/${dispute.disputeID}) created on Ethereum (Mainnet)!
+                
+    Arbitrable ${responseTag.length > 0 ? `[${responseTag[0].key1} / ${responseTag[0].key2}](${responseTag[0].key3})` : `[${dispute.arbitrable.id}](https://etherscan.io/address/${dispute.arbitrable.id})`}
+    Subcourt: ${res.data.name}
+    # of juror: ${dispute.numberOfVotes}`,
+                parse_mode: 'Markdown',
+                disable_web_page_preview: true
+                });
+    
+        }
 
-        await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-            chat_id: process.env.NOTIFICATION_CHANNEL,
-            text: "New Kleros Dispute!" + JSON.stringify(data),
-            });
+        await datalake.from(`bot-block-heights`).update({timestamp: startTimeMax}).eq("bot_name", `tg-court-functions`).eq("network", "ethereum")
         
-        
-
         return {
-            headers,
             statusCode: StatusCodes.OK
         };
     } catch (err: any) {
+        console.log(err)
         return {
           statusCode: StatusCodes.BAD_REQUEST,
           body: JSON.stringify({ error: err.message }),
