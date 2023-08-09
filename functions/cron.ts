e40f5a1d1874c5db52a6b7b895b8ba777a195cff
shotaronowhere <shawtarohgn@gmail.com>
2023-08-09 14:50:57 +0100

    chore: debugging

--- functions/cron.ts
+++ functions/cron.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 const { schedule } = require('@netlify/functions');
 
-exports.handler = schedule('* * * * *', () => {
+exports.handler = schedule('* * * * *', async () => {
 
-    axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+    await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
     chat_id: process.env.NOTIFICATION_CHANNEL,
     text: "New Kleros Dispute!",
     });
