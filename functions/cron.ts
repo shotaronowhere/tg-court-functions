9c7ca8a8e6a8c8a65cb605c6d45f1baf85cb7a83
shotaronowhere <shawtarohgn@gmail.com>
2023-08-09 14:34:07 +0100

    feat: cron

---
+++ functions/cron.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+const { schedule } = require('@netlify/functions');
+
+exports.handler = schedule('*/5 * * * *', () => {
+
+    axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+    chat_id: process.env.NOTIFICATION_CHANNEL,
+    text: "New Kleros Dispute!",
+    });
+
+    return { statusCode: 200 };
+});
