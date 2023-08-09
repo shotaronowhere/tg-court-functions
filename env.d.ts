d300838eb3a765539f4e086c741ffb76c7be188e
shotaronowhere <shawtarohgn@gmail.com>
2023-08-09 11:30:16 +0100

    feat: init

---
+++ env.d.ts
@@ -0,0 +1,13 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DATALAKE_URL: string;
+      DATALAKE_KEY: string;
+      BOT_TOKEN: string;
+      LOGTAIL_SOURCE_TOKEN: string;
+      PRIVATE_RPC_ENDPOINT_MAINNET: string;
+    }
+  }
+}
+
+export {}
