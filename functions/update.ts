02d56e56e89b1a9f1f8de3a2c3509e6fe78cccdc
shotaronowhere <shawtarohgn@gmail.com>
2023-08-10 19:22:48 +0100

    feat: juror address interaction

--- functions/update.ts
+++ functions/update.ts
@@ -1,12 +1,39 @@
 import axios from "axios";
+import ethers from "ethers";
+import { datalake } from "../config/supabase";
+
+const regexp = /\/start/
 
 exports.handler = async (event: any) => {
-    console.log("Received an update from Telegram!", event.body);
+    console.log(event.body)
+    const msg = JSON.parse(event.body).message;
 
-    await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-    chat_id: JSON.parse(event.body).message.chat.id,
-    text: "I got your message!",
-    });
+    const tg_user_id = msg.from.id;
+
+    if(msg.text.length == 42 && msg.text.startsWith("0x")){
+        if(!ethers.isAddress(msg.text)){
+            await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+                chat_id: tg_user_id,
+                text: "This is not a valid Ethereum address.",
+                });
+            return { statusCode: 200 };
+        } else {
+            await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+                chat_id: tg_user_id,
+                text: "I will notify you when a dispute is created for this address. You can change the address at any time by sending me a new one.",
+                });
+            await datalake.from(`tg-notifications-hermes`).upsert({tg_user_id: tg_user_id, juror_address: msg.text})
+            return { statusCode: 200 };
+        }
+    }
+
+        // check regex
+        if(regexp.test(msg.text)){
+            await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+                chat_id: JSON.parse(event.body).message.chat.id,
+                text: "Hi! My name is Hermes, the Kleros Messenger.\n\nPlease send me the juror address for which you would like me to notify you about.",
+            });
+        }
 
     return { statusCode: 200 };
 };
