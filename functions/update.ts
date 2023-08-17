2d5227d9a1759baa7325a0551fdfa6c346f883e5
shotaronowhere <shawtarohgn@gmail.com>
2023-08-17 12:19:11 +0100

    feat: grammy

--- functions/update.ts
+++ functions/update.ts
@@ -1,39 +1,22 @@
 import axios from "axios";
 import { isAddress } from "ethers";
 import { datalake } from "../config/supabase";
+require('dotenv').config()
+import { Bot, webhookCallback } from 'grammy'
+const { BOT_TOKEN, FUNCTION_SECRET } = process.env
+const bot = new Bot(process.env.BOT_TOKEN)
 
-const regexp = /\/start/
+const handleUpdate = webhookCallback(bot, 'aws-lambda')
+bot.command('start', ctx => ctx.reply('Hello!'))
 
 exports.handler = async (event: any) => {
-    console.log(event.body)
-    const msg = JSON.parse(event.body).message;
-
-    const tg_user_id = msg.from.id;
-
-    if(msg.text.length == 42 && msg.text.startsWith("0x")){
-        if(!isAddress(msg.text)){
-            await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-                chat_id: tg_user_id,
-                text: "This is not a valid Ethereum address.",
-                });
-            return { statusCode: 200 };
-        } else {
-            await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-                chat_id: tg_user_id,
-                text: "I will notify you when a dispute is created for this address. You can change the address at any time by sending me a new one.",
-                });
-            await datalake.from(`tg-notifications-hermes`).upsert({tg_user_id: tg_user_id, juror_address: msg.text})
-            return { statusCode: 200 };
-        }
+    try {
+        const url = new URL(event.url)
+        if (url.searchParams.get('secret') !== FUNCTION_SECRET)
+          return new Response('not allowed', { status: 405 })
+    
+        return await handleUpdate(event)
+    } catch (err) {
+    console.error(err)
     }
-
-        // check regex
-        if(regexp.test(msg.text)){
-            await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-                chat_id: JSON.parse(event.body).message.chat.id,
-                text: "Hi! My name is Hermes, the Kleros Messenger.\n\nPlease send me the juror address for which you would like me to notify you about.",
-            });
-        }
-
-    return { statusCode: 200 };
-};
+}
