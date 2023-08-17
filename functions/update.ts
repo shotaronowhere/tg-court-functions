a0169e69737809ccde5af0b7e7551bd133d6ed03
shotaronowhere <shawtarohgn@gmail.com>
2023-08-17 12:30:10 +0100

    chore: log

--- functions/update.ts
+++ functions/update.ts
@@ -4,13 +4,14 @@ import { datalake } from "../config/supabase";
 require('dotenv').config()
 import { Bot, webhookCallback } from 'grammy'
 const { BOT_TOKEN, FUNCTION_SECRET } = process.env
-const bot = new Bot(process.env.BOT_TOKEN)
+const bot = new Bot(BOT_TOKEN)
 
 const handleUpdate = webhookCallback(bot, 'aws-lambda')
 bot.command('start', ctx => ctx.reply('Hello!'))
 
 exports.handler = async (event: any) => {
     try {
+        console.log(event)
         const url = new URL(event.url)
         if (url.searchParams.get('secret') !== FUNCTION_SECRET)
           return new Response('not allowed', { status: 405 })
