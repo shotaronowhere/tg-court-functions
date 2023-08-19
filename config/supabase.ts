2bdcf50682d1ec1e001c5cb0c5c6e59d984b42d4
shotaronowhere <shawtarohgn@gmail.com>
2023-08-19 04:06:20 +0100

    chore: no local persistance

--- config/supabase.ts
+++ config/supabase.ts
@@ -3,11 +3,11 @@ import { createClient } from "@supabase/supabase-js";
 export const datalake = createClient(
   process.env.DATALAKE_URL,
   process.env.DATALAKE_KEY,
-  {auth: {persistSession: true}}
+  {auth: {persistSession: false}}
 );
 
 export const notificationSystem = createClient(
   process.env.NOTIFICATION_URL,
   process.env.NOTIFICATION_KEY,
-  {auth: {persistSession: true}}
+  {auth: {persistSession: false}}
 );
