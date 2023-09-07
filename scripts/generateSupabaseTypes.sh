b5e76c898fa9192af5b8a76496307fd48128b501
shotaronowhere <shawtarohgn@gmail.com>
2023-09-07 08:44:11 -0700

    chore: postinstall generate

--- scripts/generateSupabaseTypes.sh
+++ scripts/generateSupabaseTypes.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# export .env variables
+export $(grep -v '^#' .env | xargs)
+
 yarn -s run supabase gen types typescript --project-id elokscrfhzodpgvaixfd --schema public > types/supabase-notification.ts
 yarn -s run supabase gen types typescript --project-id pyyfdntuqbsfquzzatkz --schema public > types/supabase-datalake.ts
 
